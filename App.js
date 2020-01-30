23bc562c103479b7b4447212aa9d01d66e0c9695
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-01-30 23:28:05 +0300

    Make post using express and body-parser

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public',express.static(path.join(__dirname,'static')));
 
 app.get("/", (request, response) => {
   response.sendfile(path.join(__dirname,'static','index.html'));
 });
 
-app.get("/test", (request, response) => {
-  response.send("Your in test route");
-});
-
-app.get("/test/:name/:age", (request, response) => {
-    console.log(request.query);
-  response.send(request.params.name + " : " + request.params.age)
+app.post('/',(request,response)=>{
+  console.log(request.body);
+  // Database stuff
+  response.send("Successfull");
 });
 
 app.listen(3005);
